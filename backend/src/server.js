3f645384539bac94c46321b54ec4fd8a019b0b9e
Mohd Aquib <akibkhan9984@gmail.com>
2025-08-11 19:36:34 +0530

    fix: update CORS origin to use localhost for development and adjust axios base URL for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: "http://localhost:5173",
     credentials: true, // allow frontend to send cookies
   })
 );
