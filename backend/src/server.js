bc609aa9b1e52f12bd70ce3483cc27c1266b5a5e
Mohd Aquib <akibkhan9984@gmail.com>
2025-08-11 19:41:48 +0530

    fix: standardize import statements and update CORS configuration for better readability

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import "dotenv/config"
-import authRoutes from './routes/auth.routes.js'
-import userRoutes from "./routes/user.route.js";;
+import express from "express";
+import "dotenv/config";
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
-import cookieParser from 'cookie-parser';
-import { connectDB } from './config/db.js';
-import cors from 'cors';
-import path from 'path';
+import cookieParser from "cookie-parser";
+import { connectDB } from "./config/db.js";
+import cors from "cors";
+import path from "path";
 
 const app = express();
 
@@ -16,14 +16,17 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true, // allow frontend to send cookies
+    origin:
+      process.env.NODE_ENV === "production"
+        ? process.env.CLIENT_URL
+        : "http://localhost:5173",
+    credentials: true,
   })
 );
 app.use(express.json());
 app.use(cookieParser());
 
-// Routes 
+// Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/chat", chatRoutes);
@@ -31,14 +34,12 @@ app.use("/api/v1/chat", chatRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-});
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  connectDB()
+  connectDB();
 });
- 
