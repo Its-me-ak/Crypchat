513f74662ea55fd0f18be144214353d0597ba11b
Mohd Aquib <akibkhan9984@gmail.com>
2025-08-11 19:14:04 +0530

    fix: update CORS origin to use CLIENT_URL and correct axios base URL for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.CLIENT_URL,
   credentials: true // allow frontend to send cookies
 }));
 app.use(express.json());
