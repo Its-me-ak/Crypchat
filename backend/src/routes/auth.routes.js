c05191ae321b5b6b02abacc2e7993a52072a636e
Mohd Aquib <akibkhan9984@gmail.com>
2025-08-11 18:29:04 +0530

    feat: restructure package.json for project setup and add build script refactor: standardize route definitions in auth.routes.js fix: update axios base URL configuration for environment handling

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,11 +4,11 @@ import { protectedRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();  
 
-router.post('/login', login);
-router.post('/signup', signup)
-router.post('/logout', logout)
-router.post('/onboarding', protectedRoute, onboarding);
-router.get('/me', protectedRoute, getUser)
+router.post("/login", login);
+router.post("/signup", signup)
+router.post("/logout", logout)
+router.post("/onboarding", protectedRoute, onboarding);
+router.get("/me", protectedRoute, getUser)
 
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
