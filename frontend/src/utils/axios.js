9c2f7b2871163b0021191d1564cb19c74b19acf1
Mohd Aquib <akibkhan9984@gmail.com>
2025-08-13 17:08:39 +0530

    fix: update axios base URL to include versioning for consistency

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const BASE_URL =
   import.meta.env.MODE === "development"
-    ? "http://localhost:5001/api"
-    : "/api";
+    ? "http://localhost:5001/api/v1"
+    : "/api/v1";
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
